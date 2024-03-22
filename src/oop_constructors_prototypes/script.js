91128ac37b7cd65b976b29c91a471aad013699e8
wuyeelok <wuyeelok@gmail.com>
2024-03-22 12:25:51 +0800

    Literal vs Butil-in Contstructor

--- src/oop_constructors_prototypes/script.js
+++ src/oop_constructors_prototypes/script.js
@@ -1,1 +1,19 @@
 console.log("OOP - Constructors & Prototypes");
+
+const strLit = "Hi";
+const strObj = new String("Yo");
+
+console.log(strLit, typeof strLit);
+console.log(strObj, typeof strObj);
+
+console.log(strLit.constructor);
+console.log(strObj.constructor);
+
+console.log(strLit instanceof String);
+console.log(strObj instanceof String);
+
+const myNum = 1;
+console.log(myNum, myNum instanceof Number);
+
+const myNumObj = new Number(2);
+console.log(myNumObj, myNumObj instanceof Number);
