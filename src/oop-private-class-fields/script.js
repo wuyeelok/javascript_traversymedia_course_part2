0d352be28644e942af9e4e1c1e6290b9d86c7943
wuyeelok <wuyeelok@gmail.com>
2024-03-24 20:05:27 +0800

    Basic examples of private class fileds & methods

--- src/oop-private-class-fields/script.js
+++ src/oop-private-class-fields/script.js
@@ -1,1 +1,63 @@
 console.log("Class Private!");
+
+class Transaction {
+  #previousBalance;
+  #type;
+  #amount;
+
+  constructor(previousBalance, type, amount) {
+    this.#previousBalance = previousBalance;
+    this.#type = type;
+    this.#amount = amount;
+  }
+}
+
+class Wallet {
+  #balance;
+  #transactions;
+
+  constructor() {
+    this.#balance = 0;
+    this.#transactions = [];
+  }
+
+  #processDeposit(amount) {
+    this.#transactions.push(new Transaction(this.#balance, "deposite", amount));
+  }
+
+  #processWithdraw(amount) {
+    this.#transactions.push(new Transaction(this.#balance, "withdraw", amount));
+  }
+
+  get balance() {
+    return this.#balance.toLocaleString("en-US");
+  }
+
+  get transactions() {
+    return this.#transactions;
+  }
+
+  deposite(amount) {
+    this.#processDeposit(amount);
+    this.#balance += amount;
+  }
+
+  withdraw(amount) {
+    if (amount > this.#balance) {
+      console.log("Not enough funds!");
+      return;
+    }
+
+    this.#processWithdraw(amount);
+    this.#balance -= amount;
+  }
+}
+
+const ali = new Wallet();
+console.log(ali);
+
+ali.deposite(10000);
+ali.withdraw(500);
+console.log(ali.balance, ali.transactions);
+ali.withdraw(99999999);
+console.log(ali.balance, ali.transactions);
